7ade037ec20f02f012473601335a51b1a0dadd70
Glyson <kauaglyson@gmail.com>
2022-06-02 23:15:56 -0300

    Modularizando a conexão do BD

--- FAKER/bd.js
+++ FAKER/bd.js
@@ -1,1 +1,17 @@
-//conexão com o banco de dados
+//Tive que usar o seguinte comando no banco de dados pois não estava conectando:
+//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'teste'; flush privileges;
+
+//CONEXÃO COM O BANCO DE DADOS
+
+const bd = function () {
+    var mysql = require('mysql');
+    const { mergeFlags } = require('mysql/lib/ConnectionConfig');
+    //Setando o servidor
+    con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "teste"
+    });
+    return con;
+}
+module.exports = bd;
