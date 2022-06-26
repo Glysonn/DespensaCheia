7dc040e9d27b386f006254596db13ccd9e68f50f
Glyson <kauaglyson@gmail.com>
2022-06-26 14:38:23 -0300

    Update scriptPopularTabela.sql

--- SQL/scriptPopularTabela.sql
+++ SQL/scriptPopularTabela.sql
@@ -1,8 +1,22 @@
----AS TABELAS SERÃO POPULADAS ATRAVÉS DO CÓDIGO FEITO EM NODE.JS.
+---AS TABELAS TAMBÉM PODEM SER POPULADAS ATRAVÉS DE DADOS ALEATÓRIOS GERADOS AUTOMATICAMENTE
 
 USE despensacheia;
+-- ---------------------------------------------------------------------------------------------------------------------
+-- 										INSERTS
+insert into contribuidor values 
+(12345678912345, 'bonanza', 'mercado', 'mercado1@gmail.com',00000000,'123456788','81900000000'),
+(22222222222222, 'mercado seu zé', 'mercado', 'mercadoseuze@gmail.com',00000001,'123456788','81900000020');
 
---- no values é passado o vetor  "beneficiarios", que contem todos os dados (em ordem) de cada atributo.
-INSERT INTO beneficiario (cpf, f_name, l_name, email, cep, senha, telefone, nascimento) VALUES ?
---- no values é passado o vetor  "contribuidores", que contem todos os dados (em ordem) de cada atributo.
-INSERT INTO contribuidor (cpf, f_name, l_name, email, cep, senha, telefone, nascimento) VALUES ?
+insert into beneficiario values
+(00000000001, 'zezinho', 'da silva', 'zezinhobonitin@gmail.com', 00200500, 423423424, 00011010134, '1998-03-03'),
+(00000000002, 'douglas', 'santos', 'abububle@gmail.com', 00203530, 42343453424, 00011014534, '2000-09-23');
+
+INSERT INTO pacote values
+(1,12345678912345, true, 'Perecivel, bebida,não perecivel',true,'suco de uva','maça','arroz','macarrao','','','','','',''),
+(2,12345678912345, false, 'não perecivel',false,'farinha','farofa','fuba','','','','','','',''),
+(3,22222222222222, true, 'perecível, não perecível',false,'farinha','farofa','massa de bolo','tomate','cenoura','macaxeira','laranja','','','');
+-- -----------------------------------------------------------------------------------------------------------------------
+
+-- 									UPDATES
+update pacote set nome_mercado = 'bonanza' where cnpj_contribuidor=12345678912345;
+update pacote set nome_mercado = 'mercado do seu zé' where cnpj_contribuidor=22222222222222;
